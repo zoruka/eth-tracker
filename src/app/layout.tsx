64ee38fbf7e269f1e9d4c5bc35078b20ef20b3c2
Felipe Mendes <zo.fmendes@gmail.com>
2024-06-20 18:25:07 -0300

    chore: add site metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,47 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: site.name,
+  metadataBase: new URL(site.url),
   description: site.description,
+  keywords: [
+    'ethereum',
+    'eth',
+    'blockchain',
+    'cryptocurrency',
+    'balance',
+    'transactions',
+    'history',
+    'account',
+  ],
+
+  authors: [
+    {
+      name: 'zoruka',
+      url: 'https://zoruka.xyz',
+    },
+  ],
+  creator: 'zoruka',
+
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: site.url,
+    title: site.name,
+    description: site.description,
+    siteName: site.name,
+  },
+
+  twitter: {
+    card: 'summary_large_image',
+    title: site.name,
+    description: site.description,
+    images: [],
+    creator: '@_zoruka',
+  },
+
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
--- src/config/site.ts
+++ src/config/site.ts
@@ -1,4 +1,5 @@
 export const site = {
   name: 'EthTracker',
+  url: 'https://ethtracker.zoruka.xyz',
   description: 'Track your Ethereum account balance and transactions with ease',
 };
